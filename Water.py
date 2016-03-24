0a4a1f10bc81d5a4ea76a0b124925ef8913ef38d
Aaron Le <aaron.le@kearsarge.org>
2016-03-24 12:24:19 -0400

    Water.py created

---
+++ Water.py
@@ -0,0 +1,15 @@
+from HardBlock import *
+
+class Water(HardBlock):
+    def __init__(self, pos=[0,0], blockSize = 25):
+        image = "Block/Block Images/water.png"
+        HardBlock.__init__(self, image, pos, blockSize)
+
+    def update(*args):
+        pass
+        
+    
+        
+        
+            
+
