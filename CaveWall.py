31aa137e96e643e0bd2d50947536f47b1211bcfd
Aaron Le <aaron.le@kearsarge.org>
2016-03-28 10:45:00 -0400

    CaveWall.py created

---
+++ CaveWall.py
@@ -0,0 +1,15 @@
+from HardBlock import *
+
+class CaveWall(HardBlock):
+    def __init__(self, pos=[0,0], blockSize = 25):
+        image = "Block/Block Images/cavewall.png"
+        HardBlock.__init__(self, image, pos, blockSize)
+
+    def update(*args):
+        pass
+        
+    
+        
+        
+            
+
