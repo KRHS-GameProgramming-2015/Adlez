075252fadd056cc0610ba2fdee2d8a28540ef17f
Aaron Le <aaron.le@kearsarge.org>
2016-04-04 12:09:34 -0400

    Menu added

---
+++ Menu.py
@@ -0,0 +1,16 @@
+import sys, pygame, math, random
+
+class Menu():
+    def __init__(self, images):
+        self.images = []
+        for image in images:
+            #print image
+            self.images += [pygame.image.load(image)]
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.originalImage = self.image
+        self.width, self.height = self.image.get_size() 
+        self.playing = False
+
+    def update():
+        pass
