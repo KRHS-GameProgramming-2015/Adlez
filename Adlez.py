ec001a269b9b65dd4e543405aed65366e1b37059
Aaron Le <aaron.le@kearsarge.org>
2016-04-06 12:05:09 -0400

    mode updates to start and how to play

--- Adlez.py
+++ Adlez.py
@@ -24,10 +24,8 @@ while True:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
-                    mode = "continue"
-                if event.key == pygame.K_2:
                     mode = "start"
-                if event.key == pygame.K_3:
+                if event.key == pygame.K_2:
                     mode = "how to play"
                 if event.key == pygame.K_q:
                     mode = "quit"
@@ -36,10 +34,37 @@ while True:
         pygame.display.flip()
         clock.tick(60)
 
-    while mode == "continue":
-
     while mode == "start":
-        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player.go("up")
+                elif event.key == pygame.K_s:
+                    player.go("down")
+                elif event.key == pygame.K_a:
+                    player.go("left")
+                elif event.key == pygame.K_d:
+                    player.go("right")
+                
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    player.go("stop up")
+                elif event.key == pygame.K_s:
+                    player.go("stop down")
+                elif event.key == pygame.K_a:
+                    player.go("stop left")
+                elif event.key == pygame.K_d:
+                    player.go("stop right")
+                
     while mode == "how to play":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+                
+            
+        
         
     while mode == "quit":
