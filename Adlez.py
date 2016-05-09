aaaa19f00449b292b63ad0fe8f6fd33f0dc32fa6
Aaron Le <aaron.le@kearsarge.org>
2016-05-09 12:17:04 -0400

    Level to level from Manpac V2

--- Adlez.py
+++ Adlez.py
@@ -151,6 +151,56 @@ while True:
         all.update(size)
         #print len(all.sprites())
         
+        #From Manpac V2
+        #if player.rect.center[0] > size[0]:
+            #lx += 1
+            #for s in unloaded.sprites():
+                #s.kill()
+            #level = Level("Levels/Map"+str(lx)+str(ly))
+            #player.rect.center = [0, player.rect.center[1]]
+        #elif player.rect.center[0] < 0:
+            #lx -= 1
+            #for s in unloaded.sprites():
+                #s.kill()
+            #level = Level("Levels/Map"+str(lx)+str(ly))
+            #player.rect.center = [size[0], player.rect.center[1]]
+        
+        #elif player.rect.center[1] > size[1]:
+            #ly += 1
+            #for s in unloaded.sprites():
+                #s.kill()
+            #level = Level("Levels/Map"+str(lx)+str(ly))
+            #player.rect.center = [player.rect.center[0], 0]
+        #elif player.rect.center[1] < 0:
+            #ly -= 1
+            #for s in unloaded.sprites():
+                #s.kill()
+            #level = Level("Levels/Map"+str(lx)+str(ly))
+            #player.rect.center = [player.rect.center[0], size[1]]
+            
+        
+        #playersHitsWalls = pygame.sprite.groupcollide(players, walls, False, False)
+        
+        #for p in playersHitsWalls:
+            #for wall in playersHitsWalls[p]:
+                #p.collideWall(wall)
+                
+                
+        #playersHitsextras = pygame.sprite.groupcollide(players, extras, False, False)
+        
+        #for p in playersHitsextras:
+            #for extra in playersHitsextras[p]:
+                #score.increase(extra.value)
+                #extra.kill()
+        
+        
+        #bgColor = r,g,b
+        #screen.fill(bgColor)
+        #dirty = all.draw(screen) 
+        #pygame.display.update(dirty)
+        #pygame.display.flip()
+        #clock.tick(60)
+        
         bgColor = r,g,b
         screen.fill(bgColor)
         dirty = all.draw(screen)
